68dac2103a227bfe2a286d17ffd1757cd5a880b0
yosshipaopao <yosshipaopao@gmail.com>
2022-12-11 22:36:40 +0900

    main

--- main.js
+++ main.js
@@ -1,20 +1,74 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, Menu, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
+
+const templateMenu = [/*
+    {
+        label: 'Edit',
+        submenu: [
+            {
+                role: 'undo',
+            },
+            {
+                role: 'redo',
+            },
+        ]
+    },
+    {
+        label: 'View',
+        submenu: [
+            {
+                label: 'Reload',
+                accelerator: 'CmdOrCtrl+R',
+                click(item, focusedWindow) {
+                    if (focusedWindow) focusedWindow.reload()
+                },
+            },
+            {
+                type: 'separator',
+            },
+            {
+                role: 'resetzoom',
+            },
+            {
+                role: 'zoomin',
+            },
+            {
+                role: 'zoomout',
+            },
+            {
+                type: 'separator',
+            },
+            {
+                role: 'togglefullscreen',
+            }
+        ]
+    }*/
+];
+
+
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        titleBarStyle: 'hidden',
         webPreferences: {
+            nodeIntegration: false,
             preload: path.join(__dirname, 'preload.js'),
         },
     });
+
+    //const menu = Menu.buildFromTemplate(templateMenu);
+    //Menu.setApplicationMenu(menu);
+    //win.maximize()
+    win.toggleDevTools();
+
+    win.loadFile('game/index.html');
+
+
     ipcMain.handle('ping', () => 'pong');
     ipcMain.handle('fullscreen', () => win.setFullScreen(true));
     ipcMain.handle('unfullscreen', () => win.setFullScreen(false));
-
-    win.loadFile('game/index.html');
-    win.toggleDevTools();
 };
 
 app.whenReady().then(() => {
